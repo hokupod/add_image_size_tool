702ab4bd3d04308fa217f04b1627bec91bae8007
hokupod <hokupod@outlook.com>
2020-07-30 10:05:22 +0900

    add no-build option (ENV DISABLE_BUILD_CONTAINER)

--- main.sh
+++ main.sh
@@ -1,16 +1,23 @@
-cd `dirname $0`
-echo 'step0: docker build'
-docker build -t hokupod/ruby_fastimage .
+cd $(dirname $0) || exit 1
+
+if [ $DISABLE_BUILD_CONTAINER -ne 1 ]; then
+    echo 'step0: docker build'
+    docker build -t hokupod/ruby_fastimage .
+fi
+
 echo 'step1: copy to docker container'
 if [ $# != 1 ]; then
     echo argument error: $*
     exit 1
 fi
-
 cp $1 target/
 
 echo 'step2: start script'
 docker run -it --mount type=bind,src=$(pwd),dst=/home hokupod/ruby_fastimage bundle exec ruby main.rb target/$(basename $1)
-cd -
 
 echo 'step3: finish script'
+echo "       output to $(dirname $0)/dest/$(basename $1)"
+echo 'step4: return to your work dir'
+cd - || exit 1
+
+exit 0
