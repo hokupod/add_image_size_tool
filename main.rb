ab130af46794fa82c98bd86ee3dc0cf441fc0639
hokupod <hokupod@outlook.com>
2020-07-31 17:36:13 +0900

    Add prefix specification of src attribute.

--- main.rb
+++ main.rb
@@ -3,18 +3,25 @@ require 'fileutils'
 require 'nokogiri'
 require 'fastimage'
 
-src = ARGV[0].chomp
-dest = %(dest/#{File.basename(src)})
-doc = Nokogiri::HTML(File.open(src))
+unless ARGV[0].nil?
+  src = ARGV[0].chomp
+  src_prefix = ARGV[1] ? ARGV[1].chomp : "https:"
 
-tags = doc.search("img").map { |tag|
+  dest = %(dest/#{File.basename(src)})
+  doc = Nokogiri::HTML(File.open(src))
+
+  tags = doc.search("img").map { |tag|
     key = tag.attributes["src"].value
-    uri = "https:#{key}"
+    uri = src_prefix + key
     width, height = FastImage.size(uri)
     { search_word: %(src="#{key}"), replace_str: %(src="#{key}" width="#{width}" height="#{height}") }
-}
+  }
 
-content = File.read(src)
-tags.uniq.each { content.gsub!(_1[:search_word], _1[:replace_str]) }
+  content = File.read(src)
+  tags.uniq.each { content.gsub!(_1[:search_word], _1[:replace_str]) }
 
-File.open(dest, "w") { _1.write content }
+  File.open(dest, "w") { _1.write content }
+else
+  puts "ERROR: File not specified."
+  exit 1
+end
